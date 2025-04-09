d8f99c3e97913419e211142d54c28fd024a9ea63
Vishal Agnihotri <vishalagni8@gmail.com>
2025-04-09 16:51:40 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome To K21 Academy Project'));
+app.get('/', (req, res) => res.send('We have added a review stage with manual approval to our continuous delivery pipeline. Now, our code has successfully changed and changes will have been reviewed and approved before they are deployed to AWS Elastic Beanstalk.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
